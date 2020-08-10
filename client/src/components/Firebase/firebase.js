addf2ffd6038f9ca95836220958a6d009e5f032d
Andrei Ushakov <ushakoan@gmail.com>
2020-08-09 20:50:48 -0700

    updated firebase config

--- client/src/components/Firebase/firebase.js
+++ client/src/components/Firebase/firebase.js
@@ -1,14 +1,25 @@
 import firebase from "firebase";
 
 const config = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_MESSAGING_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey:
+    process.env.REACT_APP_FIREBASE_API_KEY ||
+    process.env.PRODUCTION_FIREBASE_API_KEY,
+  authDomain:
+    process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ||
+    "todos-app-145c3.firebaseapp.com",
+  databaseURL:
+    process.env.REACT_APP_FIREBASE_DATABASE_URL ||
+    "https://todos-app-145c3.firebaseio.com",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "todos-app-145c3",
+  storageBucket:
+    process.env.REACT_APP_FIREBASE_STORAGE_BUCKET ||
+    "todos-app-145c3.appspot.com",
+  messagingSenderId:
+    process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "538749655285",
+  appId:
+    process.env.REACT_APP_FIREBASE_MESSAGING_APP_ID ||
+    "1:538749655285:web:da44081b5e4c99f5b98dbc",
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "KGQPFNTCMF",
 };
 
 const Firebase = () => {
