418c3f450a11d34792eebd18016d632e79e731e9
Andrei Ushakov <ushakoan@gmail.com>
2020-08-10 20:07:49 -0700

    Updated CORS rules

--- api/src/index.js
+++ api/src/index.js
@@ -5,7 +5,7 @@ import routes from "./routes";
 import models, { sequelize } from "./models";
 
 const app = express();
-app.use(cors());
+app.use(cors({ origin: "https://todo-app-by-andrei.herokuapp.com/" }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
